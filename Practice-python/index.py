a7ccba36fa8beb5bf1eb7761a22723d09ba0966b
dilli <dillihangrae@gmail.com>
2021-07-14 19:45:53 +0545

    [Add] adds new folder for practicing python only

---
+++ Practice-python/index.py
@@ -0,0 +1,20 @@
+
+print("hello i am now starting and practicing python3")
+
+#data types in python
+#interger
+
+int = 45
+print(int)
+type(int)
+
+a = 500
+print(a)
+type(a)
+#id returns the variables identity with unique and precise value
+id(a)
+
+#int can be divided into three binary ,he x & octa
+binary = 01010 #value is 520
+print(binary)
+type(binary)
