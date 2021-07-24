916fade912d4af6e48bfbb8432cc263846c1aeb3
dilli <dillihangrae@gmail.com>
2021-07-24 20:38:25 +0545

    [add] adds python lessons are added

---
+++ Training Session/python/python-decorators/index.py
@@ -0,0 +1,17 @@
+
+
+# def greet(msg):
+#     def print_message():
+
+#         print(msg)
+
+#      return print_message
+
+
+#   message = greet("Hello world")
+#   message()
+
+
+  """
+  decorator funciton example
+  """
