3edf9e1d8bcf702d45eb0f8e4042b792e4a110f2
dilli <dillihangrae@gmail.com>
2021-07-24 22:54:58 +0545

    [add] adds python lessons are added

---
+++ Training Session/python/python-descriptors/index.py
@@ -0,0 +1,16 @@
+
+
+class MyClass:
+     def getter(self) -> object:
+        print("accessing the attribute to get the value")
+        return 100
+
+        def setter(self, value) -> None:
+            print("accessing the attribute to set the value")
+            raise AttributeError("Cannot change the value")
+
+        attr1 = property(getter, setter)
+        
+     obj = MyClass()
+     x = obj.attr1
+     print(x)
