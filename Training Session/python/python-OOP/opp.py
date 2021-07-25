3e9141d612da4b228ca7c03aae790ba9718dd168
dilli <dillihangrae@gmail.com>
2021-07-25 21:59:12 +0545

    [add] adds python new lessons are added

--- Training Session/python/python-OOP/opp.py
+++ Training Session/python/python-OOP/opp.py
@@ -8,23 +8,46 @@
 
    # combining all the things at together
 
-class Vehicle:
-    doors = 4
+# class Vehicle:
+#     doors = 4
 
-    def __init__(self, color, mileage):
-          self.color = color
-          self.mileage = mileage
+#     def __init__(self, color, mileage):
+#           self.color = color
+#           self.mileage = mileage
 
-          def make_it_blue(self):
-               self.color = "blue"
+#           def make_it_blue(self):
+#                self.color = "blue"
 
         
-    def get_info(self):
-            return f"{self.color} vehicle has mileage {self.mileage}"
+#     def get_info(self):
+#             return f"{self.color} vehicle has mileage {self.mileage}"
 
-            Vehicle1 = Vehicle("red", 25)
-            vehicle2 = Vehicle("white", 20)
+#             Vehicle1 = Vehicle("red", 25)
+#             vehicle2 = Vehicle("white", 20)
 
-            print(Vehicle1.color)
-            print(Vehicle1.mileage)
+#             print(Vehicle1.color)
+#             print(Vehicle1.mileage)
    
+
+
+print("hello world")
+
+class A:
+    def __init__(self, n):
+        self.n = n
+
+        def info(self):
+            return 'info method from class A'
+
+            def result(self):
+                return self.n * self.n
+        
+class B(A):
+    def result(self):
+        #calling base class method
+        result_from_base = A.result(self)
+        # modifying on super class's behaviour
+        return -result_from_base
+
+        obj_a = A(2)
+        print(obj_a.info())
