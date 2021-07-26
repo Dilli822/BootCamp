acc730cffe7bac0d69fc9930b81898719094d4ca
dilli <dillihangrae@gmail.com>
2021-07-26 08:39:37 +0545

    [add] adds python new lessons are added

---
+++ Training Session/python/class-static-method/class.py
@@ -0,0 +1,13 @@
+
+class Vehicle:
+
+    @staticmethod
+    def is_good_mileage(mileage):
+
+        if mileage < 25:
+            return "NO"
+            return "Yes"
+
+            print(Vehicle.is_good_mileage(25))
+            print(Vehicle.is_good_mileage(15))
+            
