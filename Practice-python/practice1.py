62f4a128cb331dc5db889583b190eff4e34c2e07
dilli <dillihangrae@gmail.com>
2021-07-28 14:15:15 +0545

    [add] adds practice python

--- Practice-python/practice1.py
+++ Practice-python/practice1.py
@@ -1,30 +1,25 @@
-class Books:
-   def __init__(self, name, price):
-    self.name = name
-    self.price = price
+# closure func
+# nested funciton
 
-book = Books("maths", 950)
-print(book.name)
-print(book.price)
+def main_func(param):
+    def nested_func():
+        print("it is now decorated")
+        param()
+    return nested_func
 
+@main_func
+def normal_func():
+    print("normal function")
+# message = main_func("this is argument of parameter")
+# message()
 
-# using lambda
-add = lambda a,b: a + b
-r = add(2, 5)
-print(r)
+# parent func must return the nested func
+# and nested func must call the param of main_func
 
-
-mul = lambda x,y: x * y
-n = mul(5, 5)
-print(n)
-
-div = lambda x,y: x % y
-d = div(50,2)
-print(d)
+normal_func()
 
 
-# using lambda
-a = lambda x,y: x + y
-b = a(45, 45)
-print(b)
+
+
+
 
