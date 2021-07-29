e13bc63cdeb5c6443cc50209cf40988cc323e3f9
dilli <dillihangrae@gmail.com>
2021-07-29 09:26:03 +0545

    [add] adds python practice file

--- Practice-python/practice1.py
+++ Practice-python/practice1.py
@@ -1,25 +1,12 @@
-# closure func
-# nested funciton
+class Vehicle:
+    name = "Ford"
 
-def main_func(param):
-    def nested_func():
-        print("it is now decorated")
-        param()
-    return nested_func
-
-@main_func
-def normal_func():
-    print("normal function")
-# message = main_func("this is argument of parameter")
-# message()
-
-# parent func must return the nested func
-# and nested func must call the param of main_func
-
-normal_func()
+    @classmethod
+    def printVehicleName(cls):
+        print('The name of vehilce is:', cls.name)
 
 
+# ignores 
 
-
-
+Vehicle.printVehicleName()
 
