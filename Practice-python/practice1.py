85381e76986a13cbf6b159cec7a9573224f03a28
dilli <dillihangrae@gmail.com>
2021-08-01 10:35:37 +0545

    practicing file added

--- Practice-python/practice1.py
+++ Practice-python/practice1.py
@@ -1,12 +1,23 @@
-class Vehicle:
-    name = "Ford"
+class mobile:
+    name = "sony x5 dual"
+    
+    def __init__(self, color, ram):
+        self.color = color
+        self.ram = ram
+    
 
     @classmethod
-    def printVehicleName(cls):
-        print('The name of vehilce is:', cls.name)
+    def printMobile(cls):
+        print("I have:",cls.name)
 
+    @staticmethod
+    def static(price):
+        print("The price of this mobile is", price)
 
-# ignores 
+mobile.static(35000)
+# this is class method calling
+mobile.printMobile()
 
-Vehicle.printVehicleName()
+my_mobile = mobile("black", "3gb")
+print(my_mobile.color)
 
