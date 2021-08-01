1dd0f9fef1f2d81aef1c24cac7f618410d64101a
dilli <dillihangrae@gmail.com>
2021-08-01 22:34:13 +0545

    [add] new django req res lessons url desigining path converters can use regular expression methods inside the req and res and reponse in django response objects sometimes even a jsonresponse response handling

--- Training Session/Django/index.py
+++ Training Session/Django/index.py
@@ -1,6 +1,6 @@
 
 #A simple view example
-from django.http import HttpResponse
+from django.http import HttpResponse, response
 import datetime
 
 from django.views.decorators.http import condition
@@ -24,3 +24,23 @@ from django.views.decorators.http import require_http_methods
 @require_http_methods(["GET", "POST"])
 def my_view(request):
     pass
+
+
+from django.http import HttpResponse
+response = HttpResponse("Here's the text of our webpage and applied one of the HttpResponse Objects method.")
+response = HttpResponse("DJano helper content type accept only text", content_type="text/plain")
+response = HttpResponse(b'Byteststrings are also accepted.')
+
+
+#examples
+response = HttpResponse()
+response.write("<p>Here is additional response.</p>")
+# do some calculations here
+response.write("<p>Here's another field for you.")
+
+# JsonResponse objects
+from django.http import JsonResponse
+response = JsonResponse({"name": "dilli"})
+response.content
+b'{"name": "dilli"}'
+response = JsonResponse([1,2,3,4,5], safe=False)
