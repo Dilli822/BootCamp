117356754e13deb8f1a14bc6420f878a3a3a17d0
dilli <dillihangrae@gmail.com>
2021-08-04 09:53:51 +0545

    [add] adds template base html files block include extends and several other tags for dynamic html templates

--- Projects/django/helloworld/templating/urls.py
+++ Projects/django/helloworld/templating/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import hello_templates
+from .views import hello_templates , hello_render
 
 urlpatterns = [
 
     # here url will be /templates/hello/
     # because root url conf is ready joined with it
-    path('hello/', hello_templates)
+    path('hello/', hello_templates),
+    path('hello-render/', hello_render),
 ]
