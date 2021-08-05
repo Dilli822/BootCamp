2ed3557b4adbec0c1942708dd4811f727c943ba8
dilli <dillihangrae@gmail.com>
2021-08-05 07:54:56 +0545

    [add] adds django practice project app development folder is added

--- Practice/django-practice/django_project/django_project/settings.py
+++ Practice/django-practice/django_project/django_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
 ]
 
 ROOT_URLCONF = 'django_project.urls'
