0b0e76bf864d03b331feea60c020e67f4d6b66c6
dilli <dillihangrae@gmail.com>
2021-08-17 21:19:46 +0545

    [add] adds updates

--- Projects/django/helloworld/djangoforms/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-
-from .views import forms_home
-
-urlpatterns = [
-    path('home', forms_home),
-]
--- Projects/django/helloworld/djangoforms/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def forms_home(request):
-    return render(request, 'djangoforms/forms.html', {})
