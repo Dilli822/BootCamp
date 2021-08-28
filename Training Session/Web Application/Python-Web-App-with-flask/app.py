ce5c4f1c80c31424e2c64226d3a5f0090ca00913
dilli <dillihangrae@gmail.com>
2021-08-28 17:23:32 +0545

    [added] adds templates files for flask app with codes written in app.py file

--- Training Session/Web Application/Python-Web-App-with-flask/app.py
+++ Training Session/Web Application/Python-Web-App-with-flask/app.py
@@ -1,13 +1,31 @@
 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///contact.db'
+db = SQLAlchemy(app)
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.String(25), nullable=False)
+    # password = db.Column(db.String(25), nullable=False)
+    # repeat_password = db.Column(db.String(25), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    
+
+    def __str__(self):
+        return f'user is {self.name}'
+
+
 @app.route('/')
 def home():
-    # return "Hello World"
+    # return "Hello World"a
     return render_template('form.html')
-
 
 
 if __name__ == '__main__':
