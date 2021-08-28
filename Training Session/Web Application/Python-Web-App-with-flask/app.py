829f4f8d037e0dcaab622c9f0c72e154bc45978a
dilli <dillihangrae@gmail.com>
2021-08-28 16:03:33 +0545

    [added] adds templates files for flask app with codes written in app.py file

---
+++ Training Session/Web Application/Python-Web-App-with-flask/app.py
@@ -0,0 +1,14 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # return "Hello World"
+    return render_template('form.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
