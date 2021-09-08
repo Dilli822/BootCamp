953f8b396043a7fc0237c7fd663883dd9a8ce074
dilli <dillihangrae@gmail.com>
2021-09-08 20:59:54 +0545

    [added] adds javascript Local Storage,SessionStorage and Cookies basics

--- Training Session/Front-End/JavaScript/Javascript Local Storage Session Storage & Cookie/script/app.js
+++ Training Session/Front-End/JavaScript/Javascript Local Storage Session Storage & Cookie/script/app.js
@@ -8,4 +8,18 @@ localStorage.setItem('name', 'Dilli');
 
 console.log(name);
 
-// 
+// for session storage
+
+function showSessionDiv(){
+    document.getElementById('session').style.display = 'block';
+    document.getElementById('session-btn').innerHTML = 'disappear me';
+    sessionStorage.setItem('display', 'true');
+}
+
+// conditions
+if(sessionStorage.getItem('display' ) === 'true'){
+    showSessionDiv();
+}
+// now sessionstorage memory will gets removed after browser closed completely.
+// setting cokkie from js within expiry date expires=60*1 for 1second for one day expires=60*60*24
+document.cookie = "username=dillihangrae; expires=60*1";
