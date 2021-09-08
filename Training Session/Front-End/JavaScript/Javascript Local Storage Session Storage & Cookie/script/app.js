a47e7d707cb21b96e5a2282881bb13aef9fcdb04
dilli <dillihangrae@gmail.com>
2021-09-08 19:20:19 +0545

    [added] adds Javascript LocalStorage,sessionstorage an cookies basics for web development

---
+++ Training Session/Front-End/JavaScript/Javascript Local Storage Session Storage & Cookie/script/app.js
@@ -0,0 +1,11 @@
+
+
+// setting the local stroage in the browser
+var name = localStorage.getItem('name');
+
+// setting key value pair in the application as localStorage
+localStorage.setItem('name', 'Dilli');
+
+console.log(name);
+
+// 
