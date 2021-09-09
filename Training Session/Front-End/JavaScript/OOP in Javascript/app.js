954c00fc2ab156fbca3a779adfaa35d21e0d8e8d
dilli <dillihangrae@gmail.com>
2021-09-09 22:47:56 +0545

    [added] adds Object Oriented Programming in Javascript

--- Training Session/Front-End/JavaScript/OOP in Javascript/app.js
+++ Training Session/Front-End/JavaScript/OOP in Javascript/app.js
@@ -9,3 +9,41 @@ class Test{
     }
 
 }
+
+
+// creating class 
+class Person{
+    // creating function inside the class
+    setDetails(name, address, contact){
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+    }
+    // creating system where we can get user's name ,address, contact
+    getName(){
+        // we can return the class from anywhere
+        return this.name
+    }
+
+    getAddress(){
+        return this.address;
+    }
+
+    getContact(){
+        return this.contact
+    }
+}
+// calling the class from anywhere
+// storing data in class param
+// calling object
+var data = new Person();
+// data input
+data.setDetails("dilli", "itahari", "9862231468");
+// printing the data 
+// now the data will display in the json format
+console.log(data);
+
+// extracting specific data
+console.log(data.name);
+console.log(data.address);
+console.log(data.contact);
