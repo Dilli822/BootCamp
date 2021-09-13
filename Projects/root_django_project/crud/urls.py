0c92e940862ff01040095e939906c15f5f92d2ac
dilli <dillihangrae@gmail.com>
2021-09-13 22:46:19 +0545

    [added] adds Django CRUD web apps part2

--- Projects/root_django_project/crud/urls.py
+++ Projects/root_django_project/crud/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
 
-from .views import detail_view_of_users, list_all_user
+from .views import create_user_info, detail_view_of_users, list_all_user
 
 urlpatterns = [
     # crud/
     path('list/', list_all_user),
     # capturing value
     path('detail/<int:user_id>/', detail_view_of_users),
+    path('create/', create_user_info),
 ]
--- Projects/root_django_project/crud/views.py
+++ Projects/root_django_project/crud/views.py
@@ -1,6 +1,7 @@
+from crud.forms import UserInfoModel
 from modelrelation.models import User
 from crud.models import UserInfo
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import UserInfo
 
 # Create your views here.
@@ -31,9 +32,33 @@ def detail_view_of_users(request, user_id):
 
 # function '
 def create_user_info(request):
+
     if request.method == 'POST':
         #form passess
-        pass
+
+        # if post method
+        form = UserInfoModel(request.POST)
+        if form.is_valid():
+
+            # printing the data
+            print(form.cleaned_data)
+            print("form is valid and printed")
+            # model form have save method which wilsave form in the db
+            form.save()
+
+            # after form validations redirect to
+            return redirect('/crud/list/')
+
+        # pass
+        else:
+            form = UserInfoModel()
+    else:
+        form = UserInfoModel()
+        
+    return render(request, 'crud/create.html', {
+        'form': form
+    
+    })
         
 
 
