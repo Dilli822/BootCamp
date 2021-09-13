6ddd5fd6900953050f2e07639909e338d4f01ad2
dilli <dillihangrae@gmail.com>
2021-09-13 14:45:56 +0545

    [added] adds django crud application with more features

--- Projects/root_django_project/crud/views.py
+++ Projects/root_django_project/crud/views.py
@@ -1,9 +1,17 @@
+from crud.models import UserInfo
 from django.shortcuts import render
+
+from .models import UserInfo
 
 # Create your views here.
 
 def list_all_user(request):
     # for storing dynamic data
-    
-    return render(request, 'crud/list.html')
+    data = UserInfo.objects.all()
+    # making set data type where we can pick up the data
+    context = {
+        'data': data
+    }
+
+    return render(request, 'crud/list.html', context=context)
 
