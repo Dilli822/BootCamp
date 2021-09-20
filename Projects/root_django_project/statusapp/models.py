9177a13c4ba8b8b27503f22b91560eab0630d272
dilli <dillihangrae@gmail.com>
2021-09-20 20:54:49 +0545

    [added] adds Django simple status app ...

--- Projects/root_django_project/statusapp/models.py
+++ Projects/root_django_project/statusapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# from user.models import User 
+from django.contrib.auth import get_user_model
+
+class StatusMessage(models.Model):
+    status = models.CharField(max_length=200)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user
--- Projects/root_django_project/statusapp/views.py
+++ Projects/root_django_project/statusapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import CreateView
+
+from .forms import StatusMessageModelForm
+class StatusMessageCreateView(CreateView):
+    form_class = StatusMessageModelForm
+    template_name = 'statusapp/'
+
