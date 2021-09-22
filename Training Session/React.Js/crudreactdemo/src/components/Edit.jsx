017f05303e5b50ee7adb658359c07b82faac652c
dilli <dillihangrae@gmail.com>
2021-09-22 19:14:05 +0545

    [added] adds CRUD based React simple app .......

--- Training Session/React.Js/crudreactdemo/src/components/Edit.jsx
+++ Training Session/React.Js/crudreactdemo/src/components/Edit.jsx
@@ -3,19 +3,36 @@ import React, { Component } from 'react';
 import { Modal, Form, Button} from 'semantic-ui-react';
 
 export default class Edit extends Component {
+    
+    state = {
+        name:"",
+        username: "",
+
+    }
+
+    onChangeHandler = (e) => {
+        this.setState({ [ e.target.name ] : e.target.value })
+    }
     render() {
+        const {  name,username } = this.state;
         return (
             // passing props as true open
-            <Modal open={true}>
+            <Modal open={false}>
                 <Modal.Header> Edit Your Name Here </Modal.Header>
             <Modal.Content>
 
                 <Form.Field>
-                <label>UserName</label>
-                <input name="username"/>
+                <label> Full Name: </label>
+                <input
+                 value = { username } 
+                 name = "username"
+                onChange={this.onChangeHandler} />
 
-                <label>User </label>
-                <input name="user"/> 
+                <label>Name: </label>
+                <input 
+                value={name}
+                name = "name"
+                onChange = {this.onChangeHandler } /> 
                 <Button type="submit" content="Submit"></Button>
                 </Form.Field>
             </Modal.Content>
