06f01e14a70cb1ec8c42e8754b169d316ec8e036
dilli <dillihangrae@gmail.com>
2021-09-26 15:11:43 +0545

    [added] adds DRF token authnication, permission classes to api............

--- Projects/root_django_project/rest/viewset_views.py
+++ Projects/root_django_project/rest/viewset_views.py
@@ -2,6 +2,9 @@
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from .serializers import InfoModelSerializer
 from .models import Info
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions  import IsAuthenticated
+from .permission import IsStaffUser
 
 # with modelview set we can  make single file api views for collective works
 # class InfoModelViewSet(ModelViewSet):
@@ -14,12 +17,21 @@ class InfoModelViewSet(ReadOnlyModelViewSet):
     serializer_class = InfoModelSerializer 
     queryset = Info.objects.all()
 
-    # we can override any method like list for get,post request method
 
-    # feature on sending email for user creation
-    # def create(self):
-    #     send_email()
-    #     super.create() 
+    # authentication_class
+    authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+    # must be staff too
+    permission_classes = [IsStaffUser]
+
+    # must have is_staff token to do this
+    def get_permissions(self):
+        if self.action == 'create' or self.action == 'update' or self.action == 'destroy':
+            permissions = [IsStaffUser]
+        else:
+            permissions = [IsAuthenticated]
+        return [permission() for permission in permissions]
 
 #http://127.0.0.1:8000/rest/
 # we can add more api url 
